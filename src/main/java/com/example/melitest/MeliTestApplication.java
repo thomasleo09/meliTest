a3c436080ee8ea489e35e386f7896faa64ecb190
Thomas Herrera <thomasleo09.tlhg@gmail.com>
2021-10-08 18:51:52 -0500

    Initial commit

---
+++ src/main/java/com/example/melitest/MeliTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.melitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeliTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeliTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/melitest/MeliTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.melitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
